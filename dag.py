b6bb640107c66806bc1871af5dc69cf4b0b29116
vigneshmitnick <85858332+vigneshmitnick@users.noreply.github.com>
2024-10-13 22:14:59 +0530

    Add files via upload

---
+++ dag.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+from airflow.providers.google.cloud.operators.datafusion import CloudDataFusionStartPipelineOperator
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 12, 18),
+    'depends_on_past': False,
+    'email': ['vigneshbabu3009@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('employee_dag',
+          default_args=default_args,
+          description='Runs an external Python script',
+          schedule_interval='@daily',
+          catchup=False)
+
+with dag:
+    run_script_task = BashOperator(
+        task_id='extract_data',
+        bash_command='python /home/airflow/gcs/dags/scripts/test.py',
+    )
+
+    start_pipeline = CloudDataFusionStartPipelineOperator(
+    location="us-central1",
+    pipeline_name="Vignesh_ETL_Pipeline",
+    instance_name="datafusion-first",
+    task_id="start_datafusion_pipeline",
+    )
+
+    run_script_task >> start_pipeline
